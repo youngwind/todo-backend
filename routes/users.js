533dd42c49b106e0ad529984209953f79b9343ce
liangshaofeng <liangshaofeng@innobuddy.com>
2016-03-29 14:13:14 +0800

    添加处理用户登录

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var is = require("is_js");
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
-
 // 用户注册
 router.post('/register', function (req, res, next) {
 
@@ -34,6 +29,38 @@ router.post('/register', function (req, res, next) {
     })
   });
 
+});
+
+// 登录
+router.post('/login', function (req, res, next) {
+  var userName = req.body.userName;
+  var password = req.body.password;
+  if (is.empty(userName)) {
+    throw new Error('缺乏用户名');
+  }
+  if (is.empty(password)) {
+    throw  new Error('缺乏密码');
+  }
+
+  // 这里处理用户登录有待优化
+  User.findAll({
+    where: {
+      username: userName,
+      password: password
+    }
+  }).then(function (data) {
+    if (is.empty(data)) {
+      next(new Error('账号密码不正确'));
+    } else {
+      res.send({
+        code: 0,
+        data: {
+          userId: data[0].dataValues.id,
+          userName: data[0].dataValues.username
+        }
+      })
+    }
+  })
 });
 
 
