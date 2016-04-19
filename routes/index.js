8852d11a0f8d29bb238e19d0ba14cb8be3389120
xieshanshan <911565773@qq.com>
2016-04-19 16:54:05 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,8 @@ router.get('/test/list', function (req, res, next) {
           "dimension": 1,
           "guide": "Wounded  is similar in meaning to ?",
           "id": 1,
-          "word":'wounded',
+          "wordId":12,
+          "word": 'wounded',
           "question": "Peeta is out there in the woods, wounded badly.",
           "type": 4
         },
@@ -51,7 +52,8 @@ router.get('/test/list', function (req, res, next) {
           "dimension": 1,
           "guide": "指导语1",
           "id": 2,
-          "word":'fortunate',
+          "wordId":14,
+          "word": 'fortunate',
           "question": "She was so fortunate to have avoided the shower that night",
           "type": 5
         },
@@ -59,7 +61,8 @@ router.get('/test/list', function (req, res, next) {
           "choice": ["选项AFKJSDFKJ", "选项B SDJF SDF ", "选项CSDF ", "选项DSDFSF "],
           "answer": 1,
           "dimension": 1,
-          "word":'fancy',
+          "wordId":16,
+          "word": 'fancy',
           "guide": "In this sentence,fancy means",
           "id": 3,
           "question": "He owne a fancy house out on Lake Agawam.",
@@ -69,6 +72,7 @@ router.get('/test/list', function (req, res, next) {
           "choice": ["选项A跨世纪的反馈", "选项B打飞机", "选项C是对方答复", "选项D阿斯蒂芬"],
           "answer": 1,
           "dimension": 1,
+          "wordId":18,
           "guide": "指导语1",
           "id": 4,
           "question": "这是题目",
@@ -78,6 +82,7 @@ router.get('/test/list', function (req, res, next) {
           "choice": ["选项A啊啊", "选项B方法", "选项C盛世嫡妃", "选项D水电费"],
           "answer": 1,
           "dimension": 1,
+          "wordId":20,
           "guide": "指导语1",
           "id": 5,
           "question": "My wife was so embarrased when she____some of our guests say they didn't like the meal she'd cooked for them.",
@@ -87,6 +92,7 @@ router.get('/test/list', function (req, res, next) {
           "choice": ["选项A请问", "选项Bv", "选项C请问", "选项D是大法官"],
           "answer": 1,
           "dimension": 1,
+          "wordId":22,
           "guide": "指导语1",
           "id": 6,
           "question": "这是题目",
@@ -113,41 +119,43 @@ router.post('/question', function (req, res, next) {
 });
 
 router.get('/word', function (req, res, next) {
-  res.send({
-    "code": 0,
-    "data": {
-      word:[
-        {
-          "name": "wounded",
-          "symbol": "[ˈfænsi]",
-          "explanation":[1,2,3],
-          "description":"Fancy can be an adjective, noun, or a verb. As an adjective, it’s the opposite of plain. The noun names something that isn’t real. When someone likes or wants something, the verb can be used: “I fancy a cup of tea.” Doesn’t that sound fancy?",
-          "id":1
-        },
-        {
-          "name": "fortunate",
-          "symbol": "[ˈfænsi]",
-          "explanation":['v.   想像； 设想； 想要； 猜想','n.  设想； 想像力； 爱好； 怪想','adj.  奇特的； 昂贵的； 高价的'],
-          "description":"Fancy can be an adjective, noun, or a verb. As an adjective, it’s the opposite of plain. The noun names something that isn’t real. When someone likes or wants something, the verb can be used: “I fancy a cup of tea.” Doesn’t that sound fancy?",
-          "id":2
-        },
-        {
-          "name": "fancy",
-          "symbol": "[ˈfænsi]",
-          "explanation":['v.   想像； 设想； 想要； 猜想','n.  设想； 想像力； 爱好； 怪想','adj.  奇特的； 昂贵的； 高价的'],
-          "description":"Fancy can be an adjective, noun, or a verb. As an adjective, it’s the opposite of plain. The noun names something that isn’t real. When someone likes or wants something, the verb can be used: “I fancy a cup of tea.” Doesn’t that sound fancy?",
-          "id":3
-        },
-        {
-          "name":"fancy",
-          "symbol": "[ˈfænsi]",
-          "explanation":['v.   想像； 设想； 想要； 猜想','n.  设想； 想像力； 爱好； 怪想','adj.  奇特的； 昂贵的； 高价的'],
-          "description":"Fancy can be an adjective, noun, or a verb. As an adjective, it’s the opposite of plain. The noun names something that isn’t real. When someone likes or wants something, the verb can be used: “I fancy a cup of tea.” Doesn’t that sound fancy?",
-          "id":4
-        }
-      ],
-    }
-  });
+  var  wordId = req.query.id;
+
+  if(wordId == 12){
+    res.send({
+      "code": 0,
+      "data": {
+        "name": "wounded",
+        "symbol": "[ˈfænsi]",
+        "explanation": [1, 2, 3],
+        "description": "Fancy can be an adjective, noun, or a verb. As an adjective, it’s the opposite of plain. The noun names something that isn’t real. When someone likes or wants something, the verb can be used: “I fancy a cup of tea.” Doesn’t that sound fancy?",
+        "id": 12
+      }
+    });
+  } else if(wordId == 14){
+    res.send({
+      "code": 0,
+      "data": {
+        "name": "fancy",
+        "symbol": "[ˈfænsi]",
+        "explanation": [1, 2, 3],
+        "description": "Fancy can be an adjective, noun, or a verb. As an adjective, it’s the opposite of plain. The noun names something that isn’t real. When someone likes or wants something, the verb can be used: “I fancy a cup of tea.” Doesn’t that sound fancy?",
+        "id": 14
+      }
+    });
+  } else {
+    res.send({
+      "code": 0,
+      "data": {
+        "name": "fortunate",
+        "symbol": "[ˈfænsi]",
+        "explanation": [1, 2, 3],
+        "description": "Fancy can be an adjective, noun, or a verb. As an adjective, it’s the opposite of plain. The noun names something that isn’t real. When someone likes or wants something, the verb can be used: “I fancy a cup of tea.” Doesn’t that sound fancy?",
+        "id": 16
+      }
+    });
+  }
+
 });
 
 module.exports = router;
